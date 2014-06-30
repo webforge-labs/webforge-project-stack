5954db6bbdaa6f969d6bfe3ed89377e435be1d13
Philipp Scheit <p.scheit@ps-webforge.com>
2014-06-30 15:11:36 +0200

    update for newest webforge-doctrine-compiler (add --extension=Serializer)

--- lib/Webforge/ProjectStack/Symfony/Console/CompileModelCommand.php
+++ lib/Webforge/ProjectStack/Symfony/Console/CompileModelCommand.php
@@ -14,7 +14,7 @@ class CompileModelCommand extends \Webforge\Console\Command\CommandAdapter {
   }
 
   public function doExecute(CommandInput $input, CommandOutput $output, CommandInteraction $interact, System $system) {
-    if ($system->passthru('webforge-doctrine-compiler orm:compile etc/doctrine/model.json src/php/') === 0) {
+    if ($system->passthru('webforge-doctrine-compiler orm:compile --extension=Serializer etc/doctrine/model.json src/php/') === 0) {
       return $system->passthru('bin'.DIRECTORY_SEPARATOR.'cli doctrine:schema:update --force --dump-sql');
     }
 
